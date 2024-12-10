f052e6f9b92e8ed5ee769f3301861e6876a068d9
swizkon <swizkon@gmail.com>
2024-12-10 23:52:15 +0100

    fghfghfg

--- src/backend/BotoGP.Api/Controllers/WeatherForecastController.cs
+++ src/backend/BotoGP.Api/Controllers/WeatherForecastController.cs
@@ -1,26 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//using Microsoft.AspNetCore.Mvc;
 
-namespace BotoGP.Api.Controllers;
+//namespace BotoGP.Api.Controllers;
 
-[ApiController]
-[Route("[controller]")]
-public class WeatherForecastController(ILogger<WeatherForecastController> logger) : ControllerBase
-{
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" 
-    };
+//[ApiController]
+//[Route("[controller]")]
+//public class WeatherForecastController(ILogger<WeatherForecastController> logger) : ControllerBase
+//{
+//    private static readonly string[] Summaries = new[]
+//    {
+//        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" 
+//    };
 
-    [HttpGet]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        var rng = new Random();
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast(DateTime.Now.AddDays(index))
-            {
-                // Date = ,
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-    }
-}
+//    [HttpGet]
+//    public IEnumerable<WeatherForecast> Get()
+//    {
+//        var rng = new Random();
+//        return Enumerable.Range(1, 5).Select(index => new WeatherForecast(DateTime.Now.AddDays(index))
+//            {
+//                // Date = ,
+//                TemperatureC = rng.Next(-20, 55),
+//                Summary = Summaries[rng.Next(Summaries.Length)]
+//            })
+//            .ToArray();
+//    }
+//}
